0290dcb6ce14375a25f171085bc1893269fdba2d
AkhimBari <akhim.bari91@gmail.com>
2022-01-09 19:22:45 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+
+class App extends Component {
+  state = {
+    inputFeld: 0
+
+
+  }
+
+
+  handleIncrement = () => {
+    let geld = document.getElementById('geld');
+    this.setState({ inputFeld: this.state.inputFeld + Number(geld.value) });
+
+    
+    /*console.log(5)
+    console.log("Akhim")
+    console.log(true); */
+  }
+
+
+
+
+
+  handleDecrement = () => {
+    let geld = document.getElementById('geld');
+    this.setState({ inputFeld: this.state.inputFeld - Number(geld.value) });
+    console.log(Number(geld.value));
+  }
+
+
+
+
+  render() {
+    return (<div className="header" className="konto">
+      <h1 id="title">Banana Bank</h1>
+      <p>Dein Girokonto</p>
+      <p className="saldo">{this.state.inputFeld}</p>
+      <input className="geldbetrag" type="number" id="geld" />
+      <div>
+        <button onClick={this.handleIncrement} className="einzahlen">Einzahlen</button>
+        <button onClick={this.handleDecrement} className="auszahlen" >Auszahlen</button>
+      </div>
+    </div>)
+  }
 }
 
 export default App;
+
+/*class  extends React.Component {
+  render() { 
+    return <div></div>;
+  }
+}
+ 
+export default ; */
